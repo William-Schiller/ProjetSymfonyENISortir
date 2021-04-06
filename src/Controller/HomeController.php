ca6a031411c3beed861a6855cb914aa7101dad63
William SCHILLER <william.schiller.07@gmail.com>
2021-04-06 16:04:48 +0200

    -R U Ready !!!

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @Route("", name="")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
