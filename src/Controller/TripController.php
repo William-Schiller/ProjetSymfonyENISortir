ebc0de5fb90b839073a727260fc1e9e26f952cea
LucileMarechal <lucilemarechal@gmail.com>
2021-04-07 16:56:38 +0200

    TripController TripRepository ListTripType

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -14,19 +14,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class TripController extends AbstractController
 {
     /**
-     *
-     * @Route("", name="list", methods={"GET/POST"})
+     * @Route("", name="list", methods={"GET","POST"})
      */
     public function list(EntityManagerInterface $entityManager)
     {
         //TODO Afficher la liste des sorties
 
         //Récupération des entités dans la BDD
-        $trip = $entityManager->getRepository('trip:index');
+        $trip = $entityManager->getRepository('App:Trip')->findAll();
 
         //Lien avec le dossier trip + index.html.twig
-        return $this->render('trip/index.html.twig', ['trip' => $trip]);
+        return $this->render('trip/index.html.twig', ['trips' => $trip]);
+
+
+        //Méthode findBy pour récupérer les sorties avec des critères de filtre et de tri
+
+        /*$tripsCampus = $this->getDoctrine()->getRepository(Campus::class)
+            ->findBy([], ['name' => 'desc']);
+        $tripsName = $this->getDoctrine()->getRepository(Trip::class)
+            ->findBy(['name']);
+        $tripsDateStart = $this->getDoctrine()->getRepository(Trip::class)
+            ->findBy(['dateStart']);
+        $tripsLimitInscription = $this->getDoctrine()->getRepository(Trip::class)
+            ->findBy(['dateLimitInscription']);
+
+        return $this->render('trip/index.html.twig', [
+            'tripsCampus' => $tripsCampus,
+            'tripsName' => $tripsName,
+            'tripsDateStart' => $tripsDateStart,
+            'dateLimitInscription' => $tripsLimitInscription
+        ]);*/
+
+
+        //Liste des campus
+        //$repository = $this->getDoctrine()->getRepository(Campus::class);
+        //$campus = $repository->findAll();
+
+        //return $this->render('trip/index.html.twig', array('campus' => $campus));
+
+
     }
+
+
+    //Récupération des sorties publiées sur chaque campus
+    //Utilisateur inscrit ou utilisateur organisateur
+    //Filtrer la liste en fonction du campus, du nom de la sortie, de la date de début et de fin
+    //Filtre avec checkbox : organisateur/rice, inscrit/e, pas inscrit/e, sortie terminée
 
 
 }
--- src/Repository/TripRepository.php
+++ src/Repository/TripRepository.php
@@ -14,6 +14,12 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class TripRepository extends ServiceEntityRepository
 {
+
+
+
+
+
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Trip::class);
