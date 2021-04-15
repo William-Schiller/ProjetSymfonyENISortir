1e777a00f17bb412bdfd22ce432a90585ebcd931
William SCHILLER <william.schiller.07@gmail.com>
2021-04-16 00:36:24 +0200

    Push de la mort !!!

--- src/Command/UpdateRolesCommand.php
+++ src/Command/UpdateRolesCommand.php
@@ -37,31 +37,43 @@ class UpdateRolesCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        //1 - permet au script de s'exécuter indéfiniment
+//        set_time_limit(0);
+        //2 - La fermeture du navigateur peut être ignorée par :
+//        ignore_user_abort(1);
+
         $io = new SymfonyStyle($input, $output);
 
         $userRepository = $this->entityManager->getRepository(Participant::class);
-        $users = $userRepository->findAll();
 
-        foreach($users as $user){
-            if($user->getAdmin()){
-                $roles = ['ROLE_ADMIN', 'ROLE_USER'];
-                $user->setRoles($roles);
-            }else{
-                if($user->getActive()){
-                    $roles = ['ROLE_USER'];
+        //Boucle infinie
+//        while(1){
+            $users = $userRepository->findAll();
+
+            foreach($users as $user){
+                if($user->getAdmin()){
+                    $roles = ['ROLE_ADMIN', 'ROLE_USER'];
                     $user->setRoles($roles);
-                } else {
-                    $roles = [];
-                    $user->setRoles($roles);
+                }else{
+                    if($user->getActive()){
+                        $roles = ['ROLE_USER'];
+                        $user->setRoles($roles);
+                    } else {
+                        $roles = [];
+                        $user->setRoles($roles);
+                    }
                 }
+                $this->entityManager->persist($user);
+                $io->success($user->getName() . ' is update');
             }
-            $this->entityManager->persist($user);
-            $io->success($user->getName() . ' is update');
-        }
 
-        $this->entityManager->flush();
+            $this->entityManager->flush();
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+            $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+            //Arrêter l'execution pendant 10 secondes
+//            sleep(10);
+//        }
 
         return Command::SUCCESS;
     }
